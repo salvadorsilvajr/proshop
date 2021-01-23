466a53ae032ee154ab821a67c1d14e963945a20b
salvadorsilvajr <salvadorsilvajr@yahoo.com>
2021-01-23 10:12:58 -0600

    backend and frontend basic done lecion 12 fetching info from backend (data/products.js)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/Homescreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
-     <h1>welcome to Proshop</h1>
-    </>
+    <Router>
+    <Header/>
+      <Container>
+        <main className='py-3'>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </main>
+      </Container>
+    
+    <Footer/>
+    </Router>
   );
 }
 
