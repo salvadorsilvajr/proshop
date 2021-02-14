f2ca06425b0529bbf50e5d04d3db0da76ce05154
salvadorsilvajr <salvadorsilvajr@yahoo.com>
2021-02-14 11:33:49 -0600

    add all the backend files functionality to register auth signIn L42

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,84 @@
+import asyncHandler from 'express-async-handler';
+import generateToken from '../utills/generateToken.js'
+import User from '../models/userModel.js'
+
+// @desc     Auth user and get token
+// @desc     POST /api/users/login
+// @desc     Public
+
+const authUser = asyncHandler(async (req, res) => {
+   const { email, password } = req.body
+   
+   const user = await User.findOne({ email })
+
+   if(user && (await user.matchPassword(password))) {
+      res.json({ 
+         _id: user._id,
+         name: user.name, 
+         email: user.email, 
+         isAdmin: user.isAdmin, 
+         token: generateToken(user._id)
+      })
+   } else {
+      res.status(401)
+      throw new Error(' Invalid email or password ')
+   }
+})
+
+// @desc     Register a new user
+// @desc     POST /api/users
+// @desc     Public
+
+const registerUser = asyncHandler(async (req, res) => {
+   const { name, email, password } = req.body
+   
+   const userExists = await User.findOne({ email })
+
+   if(userExists) {
+      res.status(400)
+      throw new Error('User already exists !!!')
+   }
+
+   const user = await User.create({
+      name, email, password,
+   })
+
+   if(user) {
+      res.status(201).json({
+         _id: user._id,
+         name: user.name, 
+         email: user.email, 
+         isAdmin: user.isAdmin, 
+         token: generateToken(user._id),
+      })
+   } else {
+      res.status(400)
+      throw new Error('Invalid user Data ...')
+   }
+
+})
+
+// @desc     Get user profile
+// @desc     GET /api/users/profile
+// @desc     Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+   const user = await User.findById(req.user._id)
+
+   if(user) {
+      res.json({
+         _id: user._id,
+         name: user.name, 
+         email: user.email, 
+         isAdmin: user.isAdmin, 
+      })
+   } else {
+      res.status(404)
+      throw new Error('User not Found')
+   }
+
+})
+
+export {
+   authUser, registerUser, getUserProfile
+}
--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,23 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import productRotes from './routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
-
+app.use(express.json())
 
 
 app.get('/', (req, res) => {
    res.send('API is running ...')
 } )
 
-app.use('/api/products', productRotes)
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
